5f3d35c5a4d61d92c05af36cd2c565736d9bb601
David Vivó Palanca <david.vivo@nexum.com>
2020-12-07 13:58:55 +0100

    DRY styled components material button properties

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -1,23 +1,36 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+const materialButtonBase = css`
+  cursor: pointer;
+  border-radius: 3px;
+  box-shadow: 0px 3px 7px rgba(0, 0, 0, 0.5);
+  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+
+  &:hover {
+    transform: scale(1.05);
+  }
+
+  &:active {
+    box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.65);
+  }
+`
 
 export const StyledLi = styled.li`
+  ${materialButtonBase};
   display: inline-block;
   padding: 1rem;
   margin: 1rem;
   text-align: center;
   border-radius: 3px;
-  box-shadow: 0px 3px 7px rgba(0, 0, 0, 0.5);
 `;
 
 export const StyledButton = styled.button`
-  cursor: pointer;
+  ${materialButtonBase};
   display: block;
   padding: .5rem;
   margin: auto;
   color: white;
   border: none;
-  border-radius: 3px;
-  box-shadow: 0px 3px 7px rgba(0, 0, 0, 0.5);
   background-color: ${props => props.backgroundColor ? 'red' : 'green'};
 `;
 

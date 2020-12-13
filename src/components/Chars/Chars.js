a288e6a28ef8c53c6b73e43e6b5cb007303b3c6e
David Vivó Palanca <david.vivo@nexum.com>
2020-12-13 12:30:02 +0100

    Change from pure to memo at chars component

--- src/components/Chars/Chars.js
+++ src/components/Chars/Chars.js
@@ -1,19 +1,17 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import CharsList from './CharsList/CharsList';
 import CharsControls from './CharsControls/CharsControls';
 import withMaterialSection from '../../hoc/withMaterialSection';
 import classes from '../../containers/App.css';
 
-class Chars extends PureComponent {
-  render() {
-    return (
-      <React.Fragment>
-        <h2>Your input</h2>
-        <CharsList />
-        <CharsControls />
-      </React.Fragment> 
-    )
-  }
+const chars = () => {
+  return (
+    <React.Fragment>
+      <h2>Your input</h2>
+      <CharsList />
+      <CharsControls />
+    </React.Fragment> 
+  )
 }
 
-export default withMaterialSection(Chars, classes.AppMaterialCard);
+export default React.memo(withMaterialSection(chars, classes.AppMaterialCard));
